ccfb9a0158a3139ce7cb88b72fcab35b762c4ec0
TsukinoRen <whitealbum1226@gmail.com>
2026-03-26 12:49:37 +0900

    Monster 초기 구현단계

--- TextConsoleRPG/Monster.cpp
+++ TextConsoleRPG/Monster.cpp
@@ -1,1 +1,77 @@
+// Monster.cpp
+
 #include "Monster.h"
+#include "Player.h"
+#include <cstdlib>
+#include <ctime>
+
+Monster::Monster(std::string name, int playerLevel)
+	: name_(name)
+{
+	static bool rendInitialized = false;
+	if (!rendInitialized)
+	{
+		srand(static_cast<unsigned int>(time(nullptr)));
+		rendInitialized = true;
+	}
+
+	int minHp = playerLevel * 20;
+	int maxHp = playerLevel * 30;
+	hp_ = rand() % (maxHp - minHp + 1) + minHp;
+
+	int minAtk = playerLevel * 5;
+	int maxAtk = playerLevel * 10;
+	attack_ = rand() % (maxAtk - minAtk + 1) + minAtk;
+
+	defense_ = playerLevel * 3;
+}
+
+std::string Monster::GetName()
+{
+	return name_;
+}
+int Monster::GetHP()
+{
+	return hp_;
+}
+int Monster::GetAttack()
+{
+	return attack_;
+}
+int Monster::GetDefense()
+{
+	return defense_;
+}
+
+void Monster::SetName(std::string name)
+{
+	this->name_ = name;
+}
+void Monster::SetHP(int hp)
+{
+	this->hp_ = hp;
+}
+void Monster::SetAttack(int attack)
+{
+	this->attack_ = attack;
+}
+void Monster::SetDefense(int defense)
+{
+	this->defense_ = defense;
+}
+void TakeDamage(Player& player)// 몬스터가 데미지를 입을 때
+{
+	// 해당 주석은 플레이어 완성 후 조정하고 지우면 사용 가능
+	//int takeDamage = player->GetAttack() - this->GetDefense()
+	//if (takeDamage <= 0)
+	//{ takeDamage = 1; }
+	//this->SetHP(this->GetHP() - takeDamage)
+}
+
+void PlayerAttack(Player& player) // 몬스터가 데미지를 입힐 때
+{
+	//int damage = this->GetAttack() - player->GetDefense()
+	//if (damage == 0)
+	//{ damage = 1; }
+	//player->SetHP(player->GetHP() - damage)
+}
--- TextConsoleRPG/Monster.h
+++ TextConsoleRPG/Monster.h
@@ -1,5 +1,30 @@
+// Monster.h
+
 #pragma once
+#include <string>
+
 class Monster
 {
-};
+public:
+	Monster(std::string name, int playerLevel);
 
+	virtual ~Monster() {};
+	virtual void TakeDamage(Player& player);
+	virtual void PlayerAttack(Player& player);
+
+	std::string GetName();
+	int GetHP();
+	int GetAttack();
+	int GetDefense();
+
+	void SetName(std::string name);
+	void SetHP(int hp);
+	void SetAttack(int attack);
+	void SetDefense(int defense);
+
+private:
+	std::string name_;
+	int hp_;
+	int attack_;
+	int defense_;
+};
