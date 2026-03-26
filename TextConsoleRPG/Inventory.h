992df7423866546dcc03112cfe3db9bdbff96a4e
권남웅\mario <mario2368@naver.com>
2026-03-26 17:54:04 +0900

    .h에 없어도 될 내용 .cpp로 옮김

--- TextConsoleRPG/Inventory.h
+++ TextConsoleRPG/Inventory.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <iostream>
 
-class Player // Player가 이런식으로 대충 만들어졌다고 가정.
+class Player // Player가 이런식으로 대충 만들어졌다고 가정. 얼마든지 갈아엎어질 수 있음.
 {
 public:
     size_t Hp = 50;
@@ -20,7 +20,6 @@ public:
 	virtual ~Item() {};
 };
 
-// 포션 사용시 효과들을 정리해놓은 클래스. 아직 전투시스템 구현 안돼 구체적인 구현 불가능.
 class UsePotion
 {
 protected:
@@ -29,50 +28,21 @@ public:
     virtual ~UsePotion() {};
 };
 
-//빨간 포션 클래스 정의.
 class RedPotion : public Item, public UsePotion
 {
 public:
     RedPotion() : Item("빨간 포션") {}
-	void ApplyEffect(Player& player) override
-	{
-      if (count > 0)
-		{
-            count--;
-			std::cout << "빨간포션을 사용하셨습니다. (남은 갯수:" << count << ")" << std::endl;
-            player.Hp += 50;
-            std::cout << "Hp가 50 회복되었습니다. 현재 Hp:" << player.Hp << std::endl;
-		}
-		else
-		{
-			std::cout << "남은 포션이 없습니다." << std::endl;
-		}
-	}
+    void ApplyEffect(Player& player) override;
 private:
     size_t count = 10;
 };
 
-// 파란 포션 클래스 정의.
 class BluePotion : public Item, public UsePotion
 {
 public:
     BluePotion() : Item("파란 포션") {}
-	void ApplyEffect(Player& player) override
-	{
-		if (count > 0)
-		{
-            count--;
-			std::cout << "파란 포션을 사용하셨습니다. (남은 갯수:" << count << ")" << std::endl;
-			player.Mp += 50;
-            std::cout << "Mp가 50 회복되었습니다. 현재 Mp:" << player.Mp << std::endl;
-		}
-		else
-		{
-			std::cout << "남은 포션이 없습니다." << std::endl;
-		}
-	}
-
-private:
+    void ApplyEffect(Player& player) override;
+	private:
     size_t count = 10;
 };
 
@@ -80,21 +50,8 @@ class AttackPotion : public Item, public UsePotion
 {
 public:
     AttackPotion() : Item("공격의 영약") {}
-    void ApplyEffect(Player& player) override
-    {
-        if (count > 0)
-        {
-            count--;
-            std::cout << "공격의 영약을 사용하셨습니다. (남은 갯수:" << count << ")" << std::endl;
-            player.Attack += 5;
-            std::cout << "공격력이 5 상승했습니다. 현재 공격력:" << player.Attack << std::endl;
-        }
-        else
-        {
-            std::cout << "남은 공격의 영약이 없습니다." << std::endl;
-        }
+    void ApplyEffect(Player& player) override;
 
-    }
 private:
     size_t count = 3;
 };
@@ -103,38 +60,19 @@ class DefensePotion : public Item, public UsePotion
 {
 public:
     DefensePotion() : Item("방어의 영약") {}
-    void ApplyEffect(Player& player) override
-    {
-        if (count > 0)
-        {
-            count--;
-            std::cout << "방어의 영역을 사용하셨습니다. (남은 갯수:" << count << ")" << std::endl;
-            player.Defense += 5;
-            std::cout << "방어력이 5 상승했습니다. 현재 방어력" << player.Defense << std::endl;
-        }
-        else
-        {
-            std::cout << "남은 방어의 영약이 없습니다." << std::endl;
-        }
-    }
-public:
+    void ApplyEffect(Player& player) override;
+private:
     size_t count = 3;
 };
 
-//인벤토리 클래스 정의.
+
 class Inventory
 {
 public:
     Item* items[40];
     size_t itemcount = 0;
 
-    Inventory()
-    {
-        for (int i = 0; i < 40; i++)
-        {
-            items[i] = nullptr;
-        }
-    }
-    void AddItem() {}// 이건 나중에 전투에서 몬스터를 죽이면 아이템을 주웠을 때 실행 예정. 아직 미구현.
+    Inventory();
+    void AddItem();// 이건 나중에 전투에서 몬스터를 죽이고 아이템을 주웠을 때 실행 예정. 아직 미구현.
 };
 
